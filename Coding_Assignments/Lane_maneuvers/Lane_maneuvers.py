9baabbe53f0a94ded14efa4b874c69ea021a137d
ibraam <er.ibraam.robil@gmail.com>
2025-09-01 23:08:18 +0300

    Coding_Assignments_Lane_maneuvers_Done

--- Coding_Assignments/Lane_maneuvers/Lane_maneuvers.py
+++ Coding_Assignments/Lane_maneuvers/Lane_maneuvers.py
@@ -1,4 +1,16 @@
 def analyze_drive(lanes: list[int]) -> tuple[int, int]:
+
+    lane_changes = 0
+    dangerous_maneuvers = 0
+
+    for i in range(1, len(lanes)):
+        if lanes[i] != lanes[i - 1]:
+            lane_changes += 1
+            if abs(lanes[i] - lanes[i - 1]) > 1:
+                dangerous_maneuvers += 1
+
+    return lane_changes, dangerous_maneuvers
+
     """
     Analyzes the driving behavior based on lane positions.
     
@@ -8,4 +20,4 @@ def analyze_drive(lanes: list[int]) -> tuple[int, int]:
     Returns:
         tuple[int, int]: (number of lane changes, number of dangerous maneuvers)
     """
-    pass
+    # pass
